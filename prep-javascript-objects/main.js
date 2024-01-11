bbbe11121d56549de7dec44eb3b7a7fb0700a998
Victoria Dixon <victoriakdixon88@gmail.com>
2024-01-11 14:37:25 +0000

    Completed prep-javascript-objects exercise.

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,13 @@
+const person = {firstName: "Victoria", lastName: "Mauriello", hobby: "Furniture restoration"}
+console.log(person)
+
+const fullName = "Victoria"+  ' '  +  "Mauriello"
+console.log(fullName)
+
+person.job = "Student coder and Mom";
+console.log(person.job)
+
+person ['previousJob'] = 'Social Worker';
+console.log(person['previousJob'])
+
+console.log(person)
